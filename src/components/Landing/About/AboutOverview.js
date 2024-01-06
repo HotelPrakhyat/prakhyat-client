8aff87b91b0a3c39a69eeae222c23faf932245af
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2024-01-06 17:44:12 +0530

    service page added

--- src/components/Landing/About/AboutOverview.js
+++ src/components/Landing/About/AboutOverview.js
@@ -15,19 +15,6 @@ export default function AboutOverview() {
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
     useEffect(()=>{
-            // window.addEventListener("scroll", function () {
-            //   const element = document.getElementById('fadeIn');
-            //   const position = element.getBoundingClientRect().top;
-            //   const screenHeight = window.innerHeight;
-            // // console.log(screenHeight , position)
-            //   if (position <= screenHeight) {
-            //     element.classList.add(styles.fadeOut)
-            //     element.classList.remove(styles.fadeIn);
-            // }
-            // else{
-            //       element.classList.add(styles.fadeIn)
-            //   }
-            // });
             gsap.registerPlugin(ScrollTrigger);
 // REVEAL //
 gsap.utils.toArray([titleRef.current]).forEach(function (elem) {
