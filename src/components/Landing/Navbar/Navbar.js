11618fd10b9218eca338357f016fbbc62344b045
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2024-01-08 19:46:59 +0530

    Navbar updated

--- src/components/Landing/Navbar/Navbar.js
+++ src/components/Landing/Navbar/Navbar.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import menu from "../../../assets/menu.svg"
-import logo from "../../../assets/logo.svg"
-import styles from "./Navbar.module.scss"
-export default function
-  Navbar({setSidebar }) {
+import React, { useEffect, useState } from 'react';
+import menu from "../../../assets/menu.svg";
+import logo from "../../../assets/logo.svg";
+import styles from "./Navbar.module.scss";
+import AOS from "aos"
+import 'aos/dist/aos.css'
+export default function Navbar({ setSidebar }) {
+  const [scrollY, setScrollY] = useState(0);
 
-  
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    // Attach the event listener when the component mounts
+    window.addEventListener('scroll', handleScroll);
+    console.log(window.scrollY)
+
+    // Detach the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <>
-
-      <div className={styles.navbar}>
+      <div className={`${styles.navbar}`}>
         <div className={styles.upper}>
-          <img src={logo} className={styles.logo} alt="icon"/>
-          <img src={menu} className={styles.menu} onClick={setSidebar} alt="icon"/>
+          <img src={logo} className={styles.logo} alt="icon" />
+          <img src={menu} className={styles.menu} onClick={setSidebar} alt="icon" />
         </div>
-        <div className={styles.lower}>
-          <p className={styles.title}>PRAKHYAT</p>
+        <div className={`${styles.lower} ${scrollY > 30 ? styles.invisible : ''}`} >
+          <p className={styles.title} >PRAKHYAT</p>
           <div className={styles.hotel}>
             <div className={styles.line}></div>
             <p className={styles.hoteltitle}>HOTEL</p>
@@ -24,7 +38,6 @@ export default function
           </div>
         </div>
       </div>
-
     </>
-  )
+  );
 }
